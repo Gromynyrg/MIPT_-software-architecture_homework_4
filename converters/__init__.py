1bff37b0bc71938be510f9fdbc6ba1ee48a84c49
gromynyrg <kykabazyca@gmail.com>
2025-04-12 03:03:19 +0300

    main.py fix

--- converters/__init__.py
+++ converters/__init__.py
@@ -1,4 +1,3 @@
-from .currency_converter import CurrencyConverter
 from .usd_cny_converter import UsdCnyConverter
 from .usd_rub_converter import UsdRubConverter
 from .usd_eur_converter import UsdEurConverter
